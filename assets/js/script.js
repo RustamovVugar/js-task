d1cbf091057d775aeed88b827ed41a284e71afe2
RustamovVugar <vugarar@code.edu.az>
2023-02-11 08:45:32 +0400

    added task Functions folder

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+// function number(n){
+    
+//     if (n%3==0 && n%7==0) {
+//         document.write("Eded 3 ve 7 bolunur")
+//     }else{
+//         document.writeln("eded 3 ve 7 bolunmur")
+//     }
+// }
+// number(21)
+
+//3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+// function sumOfAll(arr) {
+//     let sum =0;
+//    for (let i = 0; i < arr.length; i++) {
+//        if(arr[i]%2==0){
+//         sum+=i;
+//         return sum;
+//        }
+//    } 
+// }
+// document.writeln(sumOfAll([1,2,3,4,5,6]));
+
+
+    
